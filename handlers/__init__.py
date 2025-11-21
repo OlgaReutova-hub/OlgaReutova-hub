b33081b28644edcf6be26fcfe9786d801640fbe9
Olga Reutova <7704544@gmail.com>
2025-11-21 12:09:37 +0300

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .start import router as start_router
-from .recipes import router as recipes_router
-from .calories import router as calories_router
-from .fallback import router as fallback_router
-
-__all__ = ['start_router', 'recipes_router', 'calories_router', 'fallback_router']
-
